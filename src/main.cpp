142609ba05e453ed0836caa5701c13dfbf6a7725
aakashsingghh <akashsingh33280@gmail.com>
2026-02-03 09:21:24 +0530

    Add monitor speed in platformio.ini file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
 #define trigPin 9
 #define echoPin 10
+
 void setup() {
     pinMode(trigPin, OUTPUT);
     pinMode(echoPin, INPUT);
-
 }
 
 void loop() {
-// write your code here
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034 / 2;
+    Serial.println("distance : ");
+    Serial.println(distance);
+    Serial.println(" cm");
+    delay(1000);
 }
